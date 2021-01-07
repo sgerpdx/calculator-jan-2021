7797084e6c26ec361d5a127b5a90c1fe7d0b3617
SamuelHGerber <henrylightfoot@gmail.com>
2021-01-07 11:54:54 -0800

    subtraction added and functional in js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
+console.log('hello world');
+
 // calculator #1: addition --
 
-const addInputOne = document.getElementById('input-one');
-const addInputTwo = document.getElementById('input-two');
+const addInputOne = document.getElementById('add-one');
+const addInputTwo = document.getElementById('add-two');
 const addButton = document.getElementById('add-button');
-const addResult = document.getElementById('result');
+const addResult = document.getElementById('add-result');
 
 console.log(addInputOne);
 console.log(addInputTwo);
@@ -15,6 +17,25 @@ addButton.addEventListener('click', () => {
     console.log('sum');
 
     addResult.textContent = sum;
+});
+
+
+// #2: subtraction --
+
+const subInputOne = document.getElementById('sub-one');
+const subInputTwo = document.getElementById('sub-two');
+const subButton = document.getElementById('sub-button');
+const subResult = document.getElementById('sub-result');
+
+console.log(subInputOne);
+console.log(subInputTwo);
+
+subButton.addEventListener('click', () => {
+    const diff = subInputOne.valueAsNumber - subInputTwo.valueAsNumber;
+
+    console.log(diff);
+
+    subResult.textContent = diff;
 });
 
 
